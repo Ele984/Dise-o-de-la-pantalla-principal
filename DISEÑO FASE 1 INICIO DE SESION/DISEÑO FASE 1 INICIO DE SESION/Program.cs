02e28e7164cb184d1c2cd2215a6e1302b09e5e5e
Ele984 <90991229+Ele984@users.noreply.github.com>
2021-09-18 17:57:52 -0600

    FASE DE DISEÑO

---
+++ DISEÑO FASE 1 INICIO DE SESION/DISEÑO FASE 1 INICIO DE SESION/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DISEÑO_FASE_1_INICIO_DE_SESION
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
